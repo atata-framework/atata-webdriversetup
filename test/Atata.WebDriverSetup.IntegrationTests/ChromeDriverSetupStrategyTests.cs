499e4adf54db5123607d9174f016f671f4139669
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-07-30 15:46:19 +0300

    Add ChromeDriverSetupStrategyTests

---
+++ test/Atata.WebDriverSetup.IntegrationTests/ChromeDriverSetupStrategyTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace Atata.WebDriverSetup.IntegrationTests
+{
+    [TestFixture]
+    public class ChromeDriverSetupStrategyTests
+    {
+        [Test]
+        public void GetInstalledBrowserVersion()
+        {
+            var sut = new ChromeDriverSetupStrategy(
+                Mock.Of<IHttpRequestExecutor>());
+
+            string result = sut.GetInstalledBrowserVersion();
+
+            TestContext.WriteLine($"Result: {result}");
+
+            result.Should().NotBeNullOrWhiteSpace();
+            result[0].Should().BeInRange('1', '9');
+        }
+    }
+}
