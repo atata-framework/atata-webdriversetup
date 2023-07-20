ede585b5a1732618291e88b82d8d43c0a6d845ed
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-07-20 08:25:12 +0200

    Update .NET version of tests project to 7.0

--- test/Atata.WebDriverSetup.IntegrationTests/PathSanitizer.cs
+++ test/Atata.WebDriverSetup.IntegrationTests/PathSanitizer.cs
@@ -6,7 +6,7 @@ namespace Atata.WebDriverSetup.IntegrationTests;
 
 public static class PathSanitizer
 {
-    private static readonly Lazy<char[]> s_invalidPathChars = new(() => Path.GetInvalidFileNameChars());
+    private static readonly Lazy<char[]> s_invalidPathChars = new(Path.GetInvalidFileNameChars);
 
     public static string SanitizeForFileName(string value) =>
         new(value.Where(x => !s_invalidPathChars.Value.Contains(x)).ToArray());
