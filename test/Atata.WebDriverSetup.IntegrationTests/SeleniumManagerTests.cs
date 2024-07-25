52d344420aeeaefe18ea92ee61254525006e52ff
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-07-25 18:02:04 +0200

    Add `SeleniumManagerTests`

---
+++ test/Atata.WebDriverSetup.IntegrationTests/SeleniumManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace Atata.WebDriverSetup.IntegrationTests;
+
+public class SeleniumManagerTests
+{
+    [Test]
+    public void Edge()
+    {
+        var options = new EdgeOptions();
+
+        string driverPath = new DriverFinder(options).GetDriverPath();
+        Console.WriteLine(driverPath);
+
+        driverPath.Should().NotBeNullOrEmpty();
+    }
+}
