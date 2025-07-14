f036358b2e7f61eedd841956cdb48744165799a3
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-07-14 18:52:55 +0200

    Enable nullable reference types feature for Atata.WebDriverSetup.EdgeDriverVersionsMapReader project

--- tools/Atata.WebDriverSetup.EdgeDriverVersionsMapReader/Components/VersionPage.cs
+++ tools/Atata.WebDriverSetup.EdgeDriverVersionsMapReader/Components/VersionPage.cs
@@ -4,7 +4,7 @@ using _ = VersionPage;
 
 public sealed class VersionPage : Page<_>
 {
-    public ControlList<GridLink<_>, _> DriverLinks { get; private set; }
+    public ControlList<GridLink<_>, _> DriverLinks { get; private set; } = null!;
 
-    public LoadingIndicator<_> LoadingIndicator { get; private set; }
+    public LoadingIndicator<_> LoadingIndicator { get; private set; } = null!;
 }
--- tools/Atata.WebDriverSetup.EdgeDriverVersionsMapReader/Components/VersionsPage.cs
+++ tools/Atata.WebDriverSetup.EdgeDriverVersionsMapReader/Components/VersionsPage.cs
@@ -4,9 +4,9 @@ using _ = VersionsPage;
 
 public sealed class VersionsPage : Page<_>
 {
-    public ControlList<GridLink<_>, _> VersionLinks { get; private set; }
+    public ControlList<GridLink<_>, _> VersionLinks { get; private set; } = null!;
 
-    public LoadingIndicator<_> LoadingIndicator { get; private set; }
+    public LoadingIndicator<_> LoadingIndicator { get; private set; } = null!;
 
-    public Button<_> Next { get; private set; }
+    public Button<_> Next { get; private set; } = null!;
 }
