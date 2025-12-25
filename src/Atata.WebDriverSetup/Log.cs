fcf613c5480d42747021bc5e18542afa7ed39792
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-12-25 22:08:36 +0100

    Refactor `Log`

--- src/Atata.WebDriverSetup/Log.cs
+++ src/Atata.WebDriverSetup/Log.cs
@@ -1,31 +1,27 @@
-﻿namespace Atata.WebDriverSetup;
+﻿#pragma warning disable IDE0022 // Use expression body for method
+
+namespace Atata.WebDriverSetup;
 
 internal static class Log
 {
     internal static void Trace(string message)
     {
 #if DEBUG
-#pragma warning disable IDE0022 // Use expression body for method
         Console.Out.WriteLine(message);
-#pragma warning restore IDE0022 // Use expression body for method
 #endif
     }
 
     internal static void Warn(Exception exception, string message)
     {
 #if DEBUG
-#pragma warning disable IDE0022 // Use expression body for method
         Warn($"{message} {exception}");
-#pragma warning restore IDE0022 // Use expression body for method
 #endif
     }
 
     internal static void Warn(string message)
     {
 #if DEBUG
-#pragma warning disable IDE0022 // Use expression body for method
         Console.Out.WriteLine(message);
-#pragma warning restore IDE0022 // Use expression body for method
 #endif
     }
 }
