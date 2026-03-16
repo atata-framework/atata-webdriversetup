c2e90a40b3af9c103e92653f6d092c99ed0bbfde
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2026-03-16 08:26:36 +0100

    Fix macOS CI tests execution

---
+++ test/Atata.WebDriverSetup.IntegrationTests/GlobalFixture.cs
@@ -0,0 +1,12 @@
+﻿namespace Atata.WebDriverSetup.IntegrationTests;
+
+[SetUpFixture]
+public sealed class GlobalFixture
+{
+    [OneTimeSetUp]
+    public void GlobalSetUp()
+    {
+        if (OSInfo.IsOSX)
+            DriverSetup.GlobalConfiguration.WithCheckCertificateRevocationList(false);
+    }
+}
