fb1ef1e04ae15bf7e4593efc5564984e66293687
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2026-06-24 10:35:28 +0200

    Fix test `FirefoxDriverSetupStrategyTests.GetDriverDownloadUrl_WithLatestVersion`

--- test/Atata.WebDriverSetup.IntegrationTests/FirefoxDriverSetupStrategyTests.cs
+++ test/Atata.WebDriverSetup.IntegrationTests/FirefoxDriverSetupStrategyTests.cs
@@ -79,7 +79,9 @@ public sealed class FirefoxDriverSetupStrategyTests
         return url.ToString();
     }
 
-    [TestCaseSource(typeof(Architectures), nameof(Architectures.All))]
+    [TestCase(Architecture.X32, ExcludePlatform = Platforms.Linux)]
+    [TestCase(Architecture.X64)]
+    [TestCase(Architecture.Arm64)]
     public async Task GetDriverDownloadUrl_WithLatestVersion(Architecture architecture)
     {
         string latestVersion = await _sut.GetDriverLatestVersionAsync();
